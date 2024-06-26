cd9e95009c90f4b075de2ffa712f3d9c5220ef81
Rxf4 <raf.tapia@duocuc.cl>
2024-06-26 13:52:53 -0400

    data.py

--- modulos/data.py
+++ modulos/data.py
@@ -1,84 +1,81 @@
 import json
 
 def cargar_inventario():
-    with open('inventario.json','r') as f:
+    with open('inventario.json', 'r') as f:
         return json.load(f)
 
 def guardar_inventario(inventario):
-    with open('inventario.json','w') as f:
+    with open('inventario.json', 'w') as f:
         json.dump(inventario, f, indent = 4)
 
 def ver_listado_de_pinturas():
     inventario = cargar_inventario()
     for pintura in inventario['pinturas']:
-        print(f'codigo: {pintura['codigo']}, tipo:{pintura['tipo']}, nombre:{pintura['nombre']}')
-
+        print(f'Código: {pintura['codigo']}, Nombre: {pintura['nombre']}')
 
 def buscar_pintura(codigo_o_nombre):
     inventario = cargar_inventario()
     for pintura in inventario['pinturas']:
-        if pintura['codigo'] == codigo_o_nombre or pintura['nombre'] == codigo_o_nombre:
-            print(f'codigo: {pintura['codigo']}, nombre:{pintura['nombre']}, {pintura['descripcion']}, cantidad:{pintura['cantidad']}, stock:{pintura['stock']}')
+        if pintura['codigo'] == 380560 or pintura['nombre'] == codigo_o_nombre:
+            print(f'Código': {pintura['codigo']}, Nombre: {pintura['nombre']}, Descripción: {pintura['descripcion']}, Cantidad: {pintura['cantidad']}')
             return
-        
-def agregrar_pintura(codigo, nombre,descripcion,cantidad):
+    print('Pintura no encontrada')
+
+def agregar_pintura(codigo, nombre, descripcion, cantidad):
+    if codigo = 380560:
+        print('Código de pintura inválido. Debe ser 380560')
+        return
     inventario = cargar_inventario()
     nueva_pintura = {'codigo': codigo, 'nombre': nombre, 'descripcion': descripcion, 'cantidad': cantidad}
     inventario['pinturas'].append(nueva_pintura)
     guardar_inventario(inventario)
 
 def eliminar_pintura(codigo):
+    continue
     inventario = cargar_inventario()
     for pintura in inventario['pinturas']:
         if pintura['codigo'] == codigo:
             inventario['pinturas'].remove(pintura)
             guardar_inventario(inventario)
-            print('pintura eliminada')
+            print("Pintura eliminada")
             return
-        print('pintura no encontrada')
+    print("Pintura no encontrada")
 
 def exportar_pinturas():
     inventario = cargar_inventario()
-    with open('pinturas.txt','w') as f:
+    with open('pinturas.txt', 'w') as f:
         for pintura in inventario['pinturas']:
-            f.write(f'codigo: {pintura['codigo']}, nombre:{pintura['nombre']}, {pintura['descripcion']}, cantidad:{pintura['cantidad']}, stock:{pintura['stock']}')
+            f.write(f'Código: {pintura['codigo']}, Nombre: {pintura['nombre']}, Descripción: {pintura['descripcion']}, Cantidad: {pintura['cantidad']}')
 
-def main():
+def menu():
     while True:
-        print("Opciones:")
-        print("1. Ver Listado de Pinturas")
-        print("2. Buscar Pintura")
-        print("3. Agregar Pintura")
-        print("4. Eliminar Pintura")
-        print("5. Exportar Pinturas")
-        print("6. Salir")
+        print('1. Ver Listado de Pinturas')
+        print('2. Buscar Pintura')
+        print('3. Agregar Pintura')
+        print('4. Eliminar Pintura')
+        print('5. Exportar Pinturas')
         
-        opcion = input("Ingrese una opción: ")
+        opcion = input('Que quieres hacer?')
         
-        if opcion == "1":
+        if opcion == '1':
             ver_listado_de_pinturas()
-        elif opcion == "2":
-            codigo_o_nombre = input("Ingrese el código o nombre de la pintura: ")
+        elif opcion == '2':
+            codigo_o_nombre = input('Ingrese el código o nombre de la pintura: ')
             buscar_pintura(codigo_o_nombre)
-        elif opcion == "3":
-            codigo = input("Ingrese el código de la pintura: ")
-            nombre = input("Ingrese el nombre de la pintura: ")
-            descripcion = input("Ingrese la descripción de la pintura: ")
-            cantidad = int(input("Ingrese la cantidad de la pintura: "))
+        elif opcion == '3':
+            codigo = int(input('Ingrese el código de la pintura: '))
+                continue
+            nombre = input('Ingrese el nombre de la pintura: ')
+            descripcion = input('Ingrese la descripción de la pintura: ')
+            cantidad = int(input('Ingrese la cantidad de la pintura: '))
             agregar_pintura(codigo, nombre, descripcion, cantidad)
-        elif opcion == "4":
-            codigo = input("Ingrese el código de la pintura: ")
+        elif opcion == '4':
+            codigo = int(input("Ingrese el código de la pintura: "))
+                continue
             eliminar_pintura(codigo)
-        elif opcion == "5":
+        elif opcion == '5':
             exportar_pinturas()
-        elif opcion == "6":
-            print("Adiós!")
             break
-        else:
-            print("Opción inválida. Intente nuevamente.")
-
-if __name__ == "__main__":
-    main()
 
 
 
